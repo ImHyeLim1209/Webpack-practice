066f4b9435d87fe04c5c6ea4a84a011eba9b0e49
ImHyeLim1209 <ihl279090@gmail.com>
2021-05-18 19:25:46 +0900

    apply loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 module.exports = {
+  mode:"development",
   entry: "./source/index.js",
   output: {
     path: path.resolve(__dirname, "public"),
     filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 }
 
